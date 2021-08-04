c8f123d471ac9c057a82b570eda4e1a28df0f9b1
Svetlana Petrovich <petrovichsvetlana@bk.ru>
2021-08-04 19:49:25 +0300

    Improving tests

--- src/main/java/pageObject/CartPage.java
+++ src/main/java/pageObject/CartPage.java
@@ -1,14 +1,27 @@
 package pageObject;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import static com.codeborne.selenide.Selenide.$;
+
+import javax.lang.model.util.Elements;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class CartPage {
 
     //BUTTONS
     private static final SelenideElement checkoutButton = $("#checkout");
-    private final SelenideElement removeButton = $("#remove-sauce-labs-backpack");
+    private static final SelenideElement removeButton = $x("//button[text()='Remove']");
+    public static ElementsCollection itemsCollection = $$(".inventory_item_name");
+    private static final SelenideElement removeBackpack = $("#remove-sauce-labs-backpack");
+    private static final SelenideElement removeBikeLight = $("#remove-sauce-labs-bike-light");
+    public static final SelenideElement cartBadge = $(".shopping_cart_badge");
+
+
+    //public static final SelenideElement cartList = (SelenideElement) $$(".cart_list");
+
     private final SelenideElement cartItemPrice = $(".inventory_item_price");
+
 
     public static void clickToCheckoutButton() {
 
@@ -16,9 +29,16 @@ public class CartPage {
 
     }
 
-    public void clickToRemoveButton() {
+    //public static void clickToRemoveButton() {
 
-        removeButton.click();
+        //removeBackpack.click();
+        //removeBikeLight.click();
+
+    //}
+    public static void clickToRemoveButton() {
+
+        items.get(1).$x("//button[text()='Remove']").click();
+
     }
 
     public void getPrice () {
--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -1,12 +1,16 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.junit5.ScreenShooterExtension;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.By;
 import pageObject.*;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.*;
+import static pageObject.CartPage.clickToRemoveButton;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -14,66 +18,115 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class Tests {
 
+    LoginPage loginPage = new LoginPage();
+    ProductsPage productsPage = new ProductsPage();
+    CartPage cartPage = new CartPage();
+    CheckoutInformationPage checkoutInformationPage = new CheckoutInformationPage();
+    CheckoutOverviewPage checkoutOverviewPage = new CheckoutOverviewPage();
+    CheckoutCompletePage checkoutCompletePage = new CheckoutCompletePage();
+
+
     @RegisterExtension
     static ScreenShooterExtension screenshotEmAll = new ScreenShooterExtension(true).to("resources/screenshots");
 
     @BeforeAll
-    public void setupLogin() {
+    public void setup() {
 
         Configuration.startMaximized = true;
-        LoginPage loginPage = new LoginPage();
-        loginPage.openLoginPage();
-        loginPage.login();
-        ProductsPage.products.shouldBe(visible);
-        Assertions.assertEquals("PRODUCTS", ProductsPage.products.getText(),"User is not log in");
 
     }
 
     @Test
     @Order(1)
-    @DisplayName("Shopping")
+    @DisplayName("Login")
 
-    public void shopping() {
+    public void Login() {
 
-        ProductsPage.addToCart();
-        CartPage.clickToCheckoutButton();
-        CheckoutInformationPage.clickContinueButton();
-        CheckoutOverviewPage.clickToFinishButton();
-        CheckoutCompletePage.thankYouOrderMessage.shouldBe(visible);
-        Assertions.assertEquals("THANK YOU FOR YOUR ORDER", CheckoutCompletePage.thankYouOrderMessage.getText(),"Error: order not completed");
-        CheckoutCompletePage.clickToBackHomeButton();
+        loginPage.openLoginPage();
+        loginPage.login();
+        productsPage.products.shouldBe(visible);
+        Assertions.assertEquals("PRODUCTS", ProductsPage.products.getText(), "User is not log in");
 
     }
 
     @Test
     @Order(2)
-    @DisplayName("Cancellation of purchase")
+    @DisplayName("Shopping")
 
-    public void cancellationOfPurchase() {
+    public void shopping() {
 
-        ProductsPage.addToCart();
-        CartPage.clickToCheckoutButton();
+        loginPage.openLoginPage();
+        loginPage.login();
+        productsPage.addToCart();
+        cartPage.clickToCheckoutButton();
         CheckoutInformationPage.clickContinueButton();
-        CheckoutOverviewPage.clickToCancelButton();
-        ProductsPage.products.shouldBe(visible);
-        Assertions.assertEquals("PRODUCTS", ProductsPage.products.getText(),"Error: purchase not canceled");
+        checkoutOverviewPage.clickToFinishButton();
+        checkoutCompletePage.thankYouOrderMessage.shouldBe(visible);
+        Assertions.assertEquals("THANK YOU FOR YOUR ORDER", CheckoutCompletePage.thankYouOrderMessage.getText(), "Error: order not completed");
 
     }
 
     @Test
     @Order(3)
+    @DisplayName("Cancellation of purchase")
+
+    public void cancellationOfPurchase() {
+
+        loginPage.openLoginPage();
+        loginPage.login();
+        productsPage.addToCart();
+        cartPage.clickToCheckoutButton();
+        CheckoutInformationPage.clickContinueButton();
+        CheckoutOverviewPage.clickToCancelButton();
+        ProductsPage.products.shouldBe(visible);
+        Assertions.assertEquals("PRODUCTS", ProductsPage.products.getText(), "Error: purchase not canceled");
+
+    }
+
+    @Test
+    @Order(4)
     @DisplayName("Removing all items from the cart")
 
     public void removingAllItemsFromTheCart() {
 
+        loginPage.openLoginPage();
+        loginPage.login();
         ProductsPage.addToCart();
+
+        CartPage.cartBadge.shouldBe(visible);
+        //CartPage.itemsCollection = CartPage.clickToRemoveButton();
+        //removeButtonCollection.first().$(By.xpath("//button[text()='Remove']")).click();
+        CartPage.clickToRemoveButton();
+
     }
+
+    @Test
+    @Order(5)
+    @DisplayName("Сalculation of the total amount of items")
+
+    public void calculationOfTheTotalAmountOfItems() {
+
+        loginPage.openLoginPage();
+        loginPage.login();
+        ProductsPage.addToCart();
+        cartPage.clickToCheckoutButton();
+        CheckoutInformationPage.clickContinueButton();
+        ElementsCollection selenideElements = CartPage.itemsCollection.shouldBe(visible);
+
+
+    }
+
+
+
+
 
     @AfterAll
-    public void tearDown() {
-        closeWebDriver();
+        public void tearDown() {
+            closeWebDriver();
+        }
     }
 
 
-    }
+
+
 
