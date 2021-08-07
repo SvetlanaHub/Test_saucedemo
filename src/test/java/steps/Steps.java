236cddd2b2886e1baa18d5799228ecee578df058
Svetlana Petrovich <petrovichsvetlana@bk.ru>
2021-08-07 22:44:57 +0300

    add Сucumber project

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,4 +1,32 @@
 package steps;
 
-public class Steps {
+import io.cucumber.java.ru.Допустим;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Тогда;
+
+import tests.Tests;
+
+public class Steps extends Tests {
+
+    @Допустим("^открыта главная страница сайта \"([^\"]*)\"$")
+
+    @И("на странице присутствует форма логина")
+    public void наСтраницеПрисутствуетФормаЛогина() {
+    }
+
+    @И("пользователь вводит имя пользователя {string} в поле {string}")
+    public void пользовательВводитИмяПользователяВПоле(String arg0, String arg1) {
+    }
+
+    @И("пользователь вводит пароль {string} в поле {string}")
+    public void пользовательВводитПарольВПоле(String arg0, String arg1) {
+    }
+
+    @И("пользователь нажимает кнопку {string}")
+    public void пользовательНажимаетКнопку(String arg0) {
+    }
+
+    @Тогда("пользователь должен успешно войти в систему и перейти на страницу товаров")
+    public void пользовательДолженУспешноВойтиВСистемуИПерейтиНаСтраницуТоваров() {
+    }
 }
