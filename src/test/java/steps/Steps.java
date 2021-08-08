d25eae1e0782b0196373bc0632e782764cb12f6b
Svetlana Petrovich <petrovichsvetlana@bk.ru>
2021-08-08 18:54:03 +0300

    add Сucumber project

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -11,22 +11,26 @@ public class Steps extends Tests {
     @Допустим("^открыта главная страница сайта \"([^\"]*)\"$")
 
     @И("на странице присутствует форма логина")
-    public void наСтраницеПрисутствуетФормаЛогина() {
+    public void onPagePresentFormLogin() {
     }
 
     @И("пользователь вводит имя пользователя {string} в поле {string}")
-    public void пользовательВводитИмяПользователяВПоле(String arg0, String arg1) {
+    public void userEntersUsernameInTheField(String arg0, String arg1) {
     }
 
     @И("пользователь вводит пароль {string} в поле {string}")
-    public void пользовательВводитПарольВПоле(String arg0, String arg1) {
+    public void userEntersPasswordInTheField(String arg0, String arg1) {
     }
 
     @И("пользователь нажимает кнопку {string}")
-    public void пользовательНажимаетКнопку(String arg0) {
+    public void userPressesTheButton(String arg0) {
     }
 
     @Тогда("пользователь должен успешно войти в систему и перейти на страницу товаров")
-    public void пользовательДолженУспешноВойтиВСистемуИПерейтиНаСтраницуТоваров() {
+    public void userMustSuccessfullyLoginToTheSystemAndGoToTheProductsPage() {
+    }
+
+    @И("пользователь вошёл в систему и перешёл на страницу товаров")
+    public void userLoggedInAndWentToTheProductsPage() {
     }
 }
